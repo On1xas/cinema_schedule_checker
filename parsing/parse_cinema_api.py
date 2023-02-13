8632ed3bd8ba9d02199c442950f9cea473e21f8b
e.aleynikov <on1xas509@gmail.com>
2023-02-13 17:47:05 +0300

    Обновление функции parse_schedule_api. Убрана запись в файл. Добавлен возврат функции, в виде списка кортежей с параметрами сеансов. Изменен ввод данных, добавлена возможность делать разные запросы по кинотеатрам и датам.

--- parsing/parse_cinema_api.py
+++ parsing/parse_cinema_api.py
@@ -1,61 +1,40 @@
+from typing import List, Tuple
+
 import requests
 from lxml import etree
 import xml.etree.ElementTree as ET
 from config.config import parsing_url_api
 import datetime
 
-#
-# theatre = input('Введите номер кинотера:\nArena - 2\nDana - 3\nPalazzo - 19\nTRINITI - 11\n')
 
-
-def parse_schedule_api(theater: str) -> None:
+def parse_schedule_api() -> list[tuple[str | None, str | None, str | None, str | None, str | None]]:
     '''
     Функция делает API запрос с атрибутом "showtimes", получает в ответ html\text от сервера в формате xml с данными сеансов кинотеатра
 
-    :param theater:
-    Обозначает номер кинотеатра:
-                                    Arena - 2
-                                    Dana - 3
-                                    Palazzo - 19
-                                    TRINITI - 11
-    :return: Функция возврашает None, создает/редактирует schedule.txt в корне проекта для дальнейшего парсинка сеансов кинотеатра
+    :return: Функция возвращает список кортежей сеансов
     '''
-    date = datetime.datetime.now()
-    url = f'{parsing_url_api}{date.date()}'
+
+    data_parsing_api = []
+
+    theatre = input(
+        'Введите номер кинотеара:\nArena - 2\nDana - 3\nPalazzo - 19\nTRINITI - 11\nЕсли хотите сделать выборку по всем кинотеатрам, ничего не вводите\n')
+    dates = input(
+        'Введите дату которую хотите проверить в формате 2023-01-01. Если хотите проверить сегодняшний день, ничего не вводите\n')
+
+    if dates == "":
+        dates = datetime.datetime.now().date()
+    if theatre == "":
+        url = f'{parsing_url_api}{dates}'
+    else:
+        url = f'{parsing_url_api}{dates}&theater={theatre}'
     request = requests.get(url)
-    with open('schedule_api.txt', 'w', encoding='utf-8') as w:
-        w.write(request.text)
-
-
-# parse_schedule_api(theatre)
-
-with open('schedule_api.txt', 'r', encoding='utf-8') as r:
-    xml = r.read()
-parser = etree.XMLParser(recover=True)
-root = ET.fromstring(xml, parser=parser)
-for i in range(1, len(root[0])):
-    print(f'{root[0][i][29].text}, {root[0][i][28].text}, {root[0][i][2].text}, {root[0][i][15].text}, {root[0][i][34].text}')
-
-    # print(f'ShowID - {root[0][i][0].text}\nStartTimeShow - {root[0][i][2].text}\nShowTitle - {root[0][i][15].text}\nEventID - {root[0][i][14].text}\nEventID - {root[0][i][14].text}\nRating - {root[0][i][21].text}\nRoom - {root[0][i][28].text}\nTheatre - {root[0][i][29].text}\nFormat - {root[0][i][34].text}\n ')
-
-
- 
-
-'''import datetime
-import requests
-import sqlite3
-db_path='./parseshowdb.db'
-def create_table(path):
-    table=f'ShowsTheatre'
-
-    con=sqlite3.connect(path)
-    cur=con.cursor()
-
-    query=f'CREATE TABLE IF NOT EXISTS {table} (id,Theatre,Room,StartTimeShow,ShowTitle,Rating,Format,Audio)'
-
-    cur.execute(query)
-    con.commit()
-    con.close()
-
-create_table(db_path)'''
-
+    # with open('schedule_api.txt', 'w', encoding='utf-8') as w:
+    #     w.write(request.text)
+    # with open('schedule_api.txt', 'r', encoding='utf-8') as r:
+    xml = request.text
+    parser = etree.XMLParser(recover=True)
+    root = ET.fromstring(xml, parser=parser)
+    for i in range(1, len(root[0])):
+        data_parsing_api.append(
+            (root[0][i][29].text, root[0][i][28].text, root[0][i][2].text, root[0][i][15].text, root[0][i][34].text))
+    return list(sorted(data_parsing_api, key=lambda x: x[2]))
