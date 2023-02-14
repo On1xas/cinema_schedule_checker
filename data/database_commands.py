ec1b8419c4598d878c5bf33c3296d78ddf263560
e.aleynikov <on1xas509@gmail.com>
2023-02-14 12:17:08 +0300

    Добавлены функции update_pivot_table обновления таблицы если добавились новые сеансы fill_pivot_spl_title - заполнение таблицы названиями плейлистов

--- data/database_commands.py
+++ data/database_commands.py
@@ -1,3 +1,4 @@
+import datetime
 import sqlite3
 
 
@@ -64,9 +65,7 @@ def insert_data_tms_sql(show):
     with open() as file:
         pass
 
-def clear_parsing_all_table():
-    connect=sqlite3.connect('..\data\database.db')
-    cur=connect.cursor()
+def clear_parsing_all_table(cur, connect):
     for i in range(1,7):
         sql=f"DELETE FROM ARENA_{i}_ROOM_API"
         cur.execute(sql)
@@ -91,20 +90,38 @@ def clear_parsing_all_table():
         sql = f'DELETE FROM TRINITI_{i}_ROOM_TMS'
         cur.execute(sql)
         connect.commit()
-    connect.close()
     print(f'{clear_parsing_all_table.__name__}***Очистка всех таблиц кинозалов выполнена успешно***')
 
 def delete_raw_sql(theathre, room,table_type, id_show):
-    connect=sqlite3.connect('..\data\database.db')
-    cur=connect.cursor()
     sql=f'DELETE FROM {theathre}_{room}_ROOM_{table_type} WHERE id = {id_show}'
     cur.execute(sql)
     connect.commit()
-    connect.close()
 
-def prepare_set_show_from_db():
+
+def prepare_set_show_from_db(cur):
     show_check = set()
     cur.execute(f'SELECT SHOW_NAME_API FROM PIVOT_SHOW_TABLE')
     for show in cur.fetchall():
         show_check.add(show[0])
     return show_check
+
+def update_pivot_table(show_set_api, show_set_db, cursor,connect_db):
+    print(f'***Обновляю информацию в сводной таблице***')
+    for show in show_set_api:
+        if show in show_set_db:
+            sql = 'UPDATE PIVOT_SHOW_TABLE SET LAST_UPDATE = ? WHERE ? = SHOW_NAME_API;'
+            cursor.execute(sql, (datetime.datetime.now(), show))
+        else:
+            sql = "INSERT INTO PIVOT_SHOW_TABLE (SHOW_NAME_API, LAST_UPDATE) VALUES (?,?)"
+            cursor.execute(sql, (show, datetime.datetime.now()))
+        connect_db.commit()
+
+def fill_pivot_spl_title(cursor,connect_db):
+    cursor.execute('SELECT * FROM PIVOT_SHOW_TABLE')
+    for show in cursor.fetchall():
+        print(show)
+        if show[2] is None:
+            spl_name=input(f'***Обнаружен фильм без данных о названии SPL. Введите название SPL к фильму {show[1]}\n')
+            sql = "UPDATE PIVOT_SHOW_TABLE SET SPL_TITLE_NAME = ? WHERE id = ?;"
+            cursor.execute(sql, (spl_name, show[0]))
+            connect_db.commit()
