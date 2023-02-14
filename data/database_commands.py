3ef28334f0f3170911c77fa2e8a31a91e944db6b
e.aleynikov <on1xas509@gmail.com>
2023-02-14 17:04:43 +0300

    Добавлена функция распределения сеансов ТМС Арены

--- data/database_commands.py
+++ data/database_commands.py
@@ -61,26 +61,27 @@ def insert_data_api_sql(show):
         if room == 'Зал 6':
             return f"INSERT INTO ARENA_6_ROOM_API (TH_NAME, ROOM, SHOW_TIME, SHOW_NAME, FORMAT) VALUES(?,?,?,?,?)"
 
-def insert_data_tms_sql(data_parsing_arena):
-    # for show in data_parsing_arena:
-    #     if show[0]=='R1 S':
-    #         cur.execute("INSERT INTO ARENA_ROOM_1 (ROOM,SHOW_TIME, SPL_NAME,CPL_TITLE) VALUES (?, ?, ?, ?)", show)
-    #         conn.commit()
-    #     elif show[0]=='R2 S 3D DA':
-    #         cur.execute("INSERT INTO ARENA_ROOM_2 (ROOM,SHOW_TIME, SPL_NAME,CPL_TITLE) VALUES (?, ?, ?, ?)", show)
-    #         conn.commit()
-    #     elif show[0]=='R3 S':
-    #         cur.execute("INSERT INTO ARENA_ROOM_3 (ROOM,SHOW_TIME, SPL_NAME,CPL_TITLE) VALUES (?, ?, ?, ?)", show)
-    #         conn.commit()
-    #     elif show[0]=='R4 S 3D DA':
-    #         cur.execute("INSERT INTO ARENA_ROOM_4 (ROOM,SHOW_TIME, SPL_NAME,CPL_TITLE) VALUES (?, ?, ?, ?)", show)
-    #         conn.commit()
-    #     elif show[0]=='R5 S':
-    #         cur.execute("INSERT INTO ARENA_ROOM_5 (ROOM,SHOW_TIME, SPL_NAME,CPL_TITLE) VALUES (?, ?, ?, ?)", show)
-    #         conn.commit()
-    #     elif show[0]=='R6 S 3D':
-    #         cur.execute("INSERT INTO ARENA_ROOM_6 (ROOM,SHOW_TIME, SPL_NAME,CPL_TITLE) VALUES (?, ?, ?, ?)", show)
-    #         conn.commit()
+def insert_data_tms_sql(data_parsing_arena,cur,conn,date=str(datetime.date.today())):
+    for show in data_parsing_arena:
+        if date in show[1]:
+            if show[0]=='R1 S':
+                cur.execute("INSERT INTO ARENA_1_ROOM_TMS (TH_NAME, ROOM, SHOW_START, SPL_TITLE, CPL_TITLE) VALUES (?, ?, ?, ?, ?)",("Silver Screen в ТРЦ Arena city", show[0], show[1], show[2], show[3]))
+                conn.commit()
+            elif show[0]=='R2 S 3D DA':
+                cur.execute("INSERT INTO ARENA_2_ROOM_TMS (TH_NAME, ROOM, SHOW_START, SPL_TITLE, CPL_TITLE) VALUES (?, ?, ?, ?, ?)",("Silver Screen в ТРЦ Arena city", show[0], show[1], show[2], show[3]))
+                conn.commit()
+            elif show[0]=='R3 S':
+                cur.execute("INSERT INTO ARENA_3_ROOM_TMS (TH_NAME, ROOM, SHOW_START, SPL_TITLE, CPL_TITLE) VALUES (?, ?, ?, ?, ?)",("Silver Screen в ТРЦ Arena city", show[0], show[1], show[2], show[3]))
+                conn.commit()
+            elif show[0]=='R4 S 3D DA':
+                cur.execute("INSERT INTO ARENA_4_ROOM_TMS (TH_NAME, ROOM, SHOW_START, SPL_TITLE, CPL_TITLE) VALUES (?, ?, ?, ?, ?)",("Silver Screen в ТРЦ Arena city", show[0], show[1], show[2], show[3]))
+                conn.commit()
+            elif show[0]=='R5 S':
+                cur.execute("INSERT INTO ARENA_5_ROOM_TMS (TH_NAME, ROOM, SHOW_START, SPL_TITLE, CPL_TITLE) VALUES (?, ?, ?, ?, ?)",("Silver Screen в ТРЦ Arena city", show[0], show[1], show[2], show[3]))
+                conn.commit()
+            elif show[0]=='R6 S 3D':
+                cur.execute("INSERT INTO ARENA_6_ROOM_TMS (TH_NAME, ROOM, SHOW_START, SPL_TITLE, CPL_TITLE) VALUES (?, ?, ?, ?, ?)",("Silver Screen в ТРЦ Arena city", show[0], show[1], show[2], show[3]))
+                conn.commit()
 
 def clear_parsing_all_table(cur, connect):
     for i in range(1,7):
