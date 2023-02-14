54c4ae203c1979fc804fdec99328c329d3cc588e
e.aleynikov <on1xas509@gmail.com>
2023-02-14 18:21:18 +0300

    Добавлены переменные путей к файлам Даны ТМС

--- config/config.py
+++ config/config.py
@@ -1,11 +1,11 @@
 parsing_url_api='https://soft.silverscreen.by:8443/wssite/webapi/xml?mode=showtimes&date='
 parsing_url_arena='http://172.20.22.1:9090'
-parsing_url_dana=''
-parsing_url_palazzo=''
-parsing_url_triniti=''
+parsing_url_dana='http://172.20.23.1:9090'
+parsing_url_palazzo='http://172.20.21.1:9090'
+parsing_url_triniti='http://172.20.24.1:9090'
 tms_password='Limon4ik'
 path_to_download=r'C:\Users\e.aleynikov\Downloads'
 source_path_arena=r'C:\Users\e.aleynikov\Downloads\Schedule - BY_SS_ArenaCity.xlsx'
-# source_path_dana
+source_path_dana=r'C:\Users\e.aleynikov\Downloads\Schedule - BY_SS_Dana.xlsx'
 # source_path_palazzo
 # source_path_triniti
