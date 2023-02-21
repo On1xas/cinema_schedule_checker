b4200e16e2fccfca68bb7dcfb34c4b3b3bd074f7
YAUHEN ALEINIKAU <96020729+On1xas@users.noreply.github.com>
2023-02-21 22:33:44 +0300

    Изменены ссылки к путям проекта через config

--- parsing/parsing_arena.py
+++ parsing/parsing_arena.py
@@ -5,14 +5,17 @@ import openpyxl
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from config.config import parsing_url_arena
+from config.config import parsing_url_arena, source_path_arena, path_to_download, project_path
 import sqlite3
+
+
 def parser_tms_arena():
     driver = webdriver.Chrome()
     driver.get(parsing_url_arena)  # Подключаемся на TMS ArenaCity
     driver.maximize_window()
     time.sleep(3)
-    element = driver.find_element(By.XPATH, '//*[@id="tms-view-body"]/div/div/div[3]/div[2]/div[2]/form/div[1]/input[2]')
+    element = driver.find_element(By.XPATH,
+                                  '//*[@id="tms-view-body"]/div/div/div[3]/div[2]/div[2]/form/div[1]/input[2]')
     element.send_keys("Limon4ik")
     driver.find_element(By.XPATH, '//*[@id="tms-view-body"]/div/div/div[3]/div[2]/div[2]/form/div[3]/button').click()
     time.sleep(1)
@@ -30,16 +33,16 @@ def parser_tms_arena():
     time.sleep(1)
     driver.close()
     driver.quit()
-    path_to_download = r'C:\Users\e.aleynikov\Downloads'
+    # path_to_download = r'C:\Users\e.aleynikov\Downloads'
     os.chdir(path_to_download)
-    source_path_arena = r'C:\Users\e.aleynikov\Downloads\Schedule - BY_SS_ArenaCity.xlsx'
+    # source_path_arena = r'C:\Users\e.aleynikov\Downloads\Schedule - BY_SS_ArenaCity.xlsx'
 
     if os.path.exists(source_path_arena):
-        destination_path = r'D:\N\Python\cinema_schedule_checker\data'
+        destination_path = fr'{project_path}\data'
         shutil.move(source_path_arena, destination_path)
     time.sleep(1)
 
-    wookbook = openpyxl.load_workbook(r'D:\N\Python\cinema_schedule_checker\data\Schedule - BY_SS_ArenaCity.xlsx')
+    wookbook = openpyxl.load_workbook(fr'{project_path}\data\Schedule - BY_SS_ArenaCity.xlsx')
     worksheet = wookbook.active
     data = []
     for i in range(1, worksheet.max_row):
@@ -49,6 +52,3 @@ def parser_tms_arena():
         del temp[2], temp[2], temp[2], temp[2], temp[2]
         data.append(tuple(temp))
     return data
-
-
-
