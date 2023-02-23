b1cb9279030f6f9d90c26035222bcd65d186549a
e.aleynikov <on1xas509@gmail.com>
2023-02-23 16:06:14 +0300

    Изменен метод получения параметра audio format Добавлены комментарии по ходу выполнения функции

--- parsing/parse_cinema_api.py
+++ parsing/parse_cinema_api.py
@@ -13,19 +13,19 @@ def parse_schedule_api():
     Функция делает API запрос с атрибутом "showtimes",
      получает в ответ html\text от сервера в формате xml с данными сеансов кинотеатра
 
-    :return: Функция возвращает список кортежей сеансов
+    :return: Функция возвращает список кортежей сеансов отсортированных по дате сеанса
     """
 
     data_parsing_api = []
     colorama.init()
     theatre = ''
-        # input(
-        # '''Введите номер кинотеара:\nArena - 2\nDana - 3\nPalazzo - 19\nTRINITI - 11\n
-        # Если хотите сделать выборку по всем кинотеатрам, ничего не вводите\n''')
+    # input(
+    # '''Введите номер кинотеара:\nArena - 2\nDana - 3\nPalazzo - 19\nTRINITI - 11\n
+    # Если хотите сделать выборку по всем кинотеатрам, ничего не вводите\n''')
     dates = ''
-        # input(
-        # '''Введите дату которую хотите проверить в формате 2023-01-01(YYYY-MM-DD).
-        # Если хотите проверить сегодняшний день, ничего не вводите\n''')
+    # input(
+    # '''Введите дату которую хотите проверить в формате 2023-01-01(YYYY-MM-DD).
+    # Если хотите проверить сегодняшний день, ничего не вводите\n''')
 
     if dates == "":
         dates = datetime.datetime.now().date()
@@ -33,7 +33,8 @@ def parse_schedule_api():
         url = f'{parsing_url_api}{dates}'
     else:
         url = f'{parsing_url_api}{dates}&theater={theatre}'
-    print(f"***Выполняю запрос сеансов на {colorama.Fore.RED} {dates} {colorama.Style.RESET_ALL} из программного обеспечения кинотеатра***")
+    print(
+        f"***Выполняю запрос сеансов на {colorama.Fore.RED} {dates} {colorama.Style.RESET_ALL} из программного обеспечения кинотеатра***")
     request = requests.get(url)
     if request.status_code == 200:
         print(f'***Ответ получен успешно***')
@@ -41,16 +42,18 @@ def parse_schedule_api():
         parser = etree.XMLParser(recover=True)
         root = ET.fromstring(xml, parser=parser)
         for i in range(1, len(root[0])):
-            if "(RU SUB)" in root[0][i][15].text:
-                audio_format="EN-RU"
-            else:
-                audio_format="RU-RU"
+            audio_format = "EN-RU" if "(RU SUB)" in root[0][i][15].text else "RU-RU"
+            # Добавляем в список, кортеж (Название кинотеатра, Название зала, Дата+Время сеанса, Название сеанса, Формат фильма, Аудиоформат фильма)
             data_parsing_api.append(
-                (
-                    root[0][i][29].text, root[0][i][28].text, root[0][i][2].text, root[0][i][15].text,
-                    root[0][i][34].text, audio_format))
+                (root[0][i][29].text, root[0][i][28].text, root[0][i][2].text, root[0][i][15].text,
+                 root[0][i][34].text, audio_format))
+            # Возвращаем отсортированный список кортежей по дате сеанса
         return list(sorted(data_parsing_api, key=lambda x: x[2]))
     else:
         print(
             f"""***Проверьте полученные данные, возможно запрос был выполнен некорректно или не выполнен вовсе. Ошибка запроса {request.status_code}.***""")
         return None
+
+
+if __name__ == '__main__':
+    pass
