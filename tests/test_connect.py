161d7b167cba4cecc0f358e4fb98d05bf1574d47
YAUHEN ALEINIKAU <96020729+On1xas@users.noreply.github.com>
2023-02-24 19:13:43 +0300

    Реализована функция check_connection

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -1,5 +1,19 @@
 from pythonping import ping
+import colorama
+
+network_data = {'PALAZZO': '172.20.21.1', 'ARENA': '172.20.22.1', 'DANA': '172.20.23.1', 'TRINITI': '172.20.24.1',
+                'INTERNET': '8.8.8.8'}
 
 
-res=ping('172.20.21.111', verbose=False, count=1)
-print(res.stats_packets_returned)
+def check_connection(thearte_name, ip):
+    colorama.init()
+    check_ping = ping(ip, verbose=False, count=1)
+    if check_ping.stats_packets_returned == 1:
+        print(f'Сетевое подключение к {thearte_name} - {colorama.Fore.GREEN}OK {colorama.Style.RESET_ALL}')
+    else:
+        print(f'Сетевое подключение к {thearte_name} - {colorama.Fore.RED}BAD {colorama.Style.RESET_ALL}')
+        #raise ConnectionError(f'Проверьте сетевое подключение к {key}')
+
+
+for key, value in network_data.items():
+    check_connection(key, value)
