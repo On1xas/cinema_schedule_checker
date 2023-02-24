9f2b30c327a48fc19e36b0bc40787e37fa6084e1
e.aleynikov <on1xas509@gmail.com>
2023-02-24 15:02:05 +0300

    Добавлена переменная rooms_tms_name

--- config/config.py
+++ config/config.py
@@ -19,3 +19,7 @@ room_names = [['Зал 1 mooon IMAX', 'Зал 2 Vegas', 'Зал 3 VIP', 'Зал 
         ['Зал 1 Premiere', 'Зал 2', 'Зал 3 Screen X', 'Зал 4 Vegas lounge', 'Зал 5', 'Зал 6 VIP', 'Зал 7 VIP'],
         ['Зал 1', 'Зал 2 Lounge', 'Зал 3 Premiere', 'Зал 4 Resto', 'Зал 5 VIP']]
 table_th_names = ['PALAZZO', 'ARENA', 'DANA', 'TRINITI']
+room_tms_names=[['R1 IMAX','R2 F','R3 F','R4 S','R5 F 3D','R6 F','R7 S'],
+                ['R1 S','R2 S 3D DA','R3 S','R4 S 3D DA','R5 S','R6 S 3D' ],
+                ['R1 S 3D DA','R2 F','R3 S SX','R4 S','R5 S 3D','R6 S','R7 S'],
+                ['R1','R2 S','R3 F 3D','R4 S','R5 S']]
