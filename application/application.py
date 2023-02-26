d542a013c1fe4fd753c4d8ed40787afeae0811b4
YAUHEN ALEINIKAU <96020729+On1xas@users.noreply.github.com>
2023-02-26 23:41:10 +0300

    Создана таблица и первый раздел вврда ID и нового названия SPL

--- application/application.py
+++ application/application.py
@@ -1,29 +1,60 @@
 import tkinter as tk
 from tkinter import ttk
+from app_func import *
+def clear_data_in_db():
+    new_id=entry_id_label.get()
+    new_spl=entry_new_spl_name.get()
+    update_pivot_raw(new_spl,new_id)
+    clear_table(table)
+    load_table_info(table)
 
-window=tk.Tk()
+
+window = tk.Tk()
 window.title('Тут будет название приложения')
 window.iconbitmap()
+window.minsize(1200, 800)
+frame_table = tk.Frame(window)
+frame_control = tk.Frame(window)
 
-frame_table=tk.Frame(window,width=200,height=200, bg='yellow')
-frame_control=tk.Frame(window,width=200,height=200, bg='green')
+frame_table.place(relx=0, rely=0, relwidth=0.5, relheight=1)
+frame_control.place(relx=0.5, rely=0, relwidth=0.5, relheight=1)
 
-frame_table.grid(row=0,column=0)
-frame_control.grid(row=0,column=1)
+tmp_data=[(3, ' Аватар: Путь воды ', 'BLUEWATERPEOPL', '2023-02-24 14:51:52.778549'), (6, ' Непослушная ', 'Naughty', '2023-02-24 14:51:52.760532'), (13, ' Чебурашка ', 'CHEBURASHKA', '2023-02-24 14:51:52.789106'), (19, ' Топ Ган: Мэверик ', 'TOPGUNMAVERICK', '2023-02-24 14:51:52.766551'), (22, ' Свободные отношения ', 'SVO', '2023-02-22 14:47:05.328507'), (23, ' Элвис ', 'Elvis', '2023-02-22 14:47:05.331517'), (24, ' Знакомство родителей ', 'MaybeIDo', '2023-02-22 14:47:05.344560'), (25, ' Кот в сапогах 2: Последнее желание ', 'PiBLastWish', '2023-02-24 14:51:52.784184'), (26, ' Астерикс и Обеликс: Поднебесная ', 'ASTERIXEMPIRE', '2023-02-24 14:51:52.786829'), (27, ' Заклятье. Первое причастие ', 'TheCommunion', '2023-02-24 14:51:52.806131'), (28, ' Амстердам ', 'Amsterdam', '2023-02-22 14:47:05.338541'), (29, ' Мой домашний крокодил ', 'LYLELYLECROCODILE', '2023-02-22 14:47:05.347572'), (33, ' Одиноки вместе ', 'AloneTogether', '2023-02-22 14:47:05.317470'), (34, ' Изумительный Морис ', 'AmazingMaurice', '2023-02-22 14:47:05.320482'), (35, ' Легион супергероев ', 'Legion', '2023-02-22 11:26:50.188535'), (36, ' Предпоказ. Рок Дог 3: Битва за бит ', 'RockDog3', '2023-02-22 14:47:05.364818'), (37, ' Кто украл Banksy (RU SUB) ', 'ManStoleBanksy', '2023-02-22 14:47:05.325497'), (38, ' Снежная королева: Разморозка ', 'SQ', '2023-02-24 14:51:52.799809'), (39, ' Спецпоказ. Сын ', 'TheSon', '2023-02-23 17:22:47.379858'), (40, ' Марлоу ', 'MARLOU', '2023-02-24 14:51:52.802818'), (41, ' TheatreHD: Курентзис: Бетховен Симфония № 9 ', 'BETH', '2023-02-23 17:22:47.366008'), (42, ' Italian Best Shorts 5: Жизнь как чудо ', 'ItBest', '2023-02-24 14:51:52.757521'), (43, ' Рок дог 3: Битва за бит ', 'RockDog3', '2023-02-24 14:51:52.775559'), (44, ' Мумиёшки ', 'a', '2023-02-24 14:51:52.781176'), (45, ' Бешенство ', 'RAGE', '2023-02-24 14:51:52.797804'), (46, ' Папы против мам ', 'fas', '2023-02-24 14:51:52.763542'), (47, ' Сын ', 'd', '2023-02-24 14:51:52.769561'), (48, ' Титаник (Перевыпуск) ', 'sdf', '2023-02-24 14:51:52.772571'), (49, ' TheatreHD: Роден ', 's', '2023-02-24 14:51:52.792114'), (50, ' Музыкотерапия. Источник ', 'f', '2023-02-24 14:51:52.794848')]
+
+# Создаем виджет Treeview (аналог таблицы)
+table=ttk.Treeview(frame_table, show='headings')
+
+# Задаем название стобцов
+heads=['id', 'SHOW_NAME', 'SPL_TITLE']
+# Присватваем параметру "colums" название столбцов
+table['columns']=heads
+
+# Передаем данные в таблицу. Вносим заголовки столбцов и выравниваем их по центру
+for head in heads:
+    table.heading(head, text=head, anchor='center')
+    # Передаем команду что данные в ячейках столбцов должна быть поцентру
+    table.column(head,  anchor='center')
+
+for row in tmp_data:
+    table.insert("",tk.END, values=row)
+scr_panel=ttk.Scrollbar(frame_table, command=table.yview)
+scr_panel.pack(side=tk.RIGHT, fill=tk.Y)
+table.configure(yscrollcommand=scr_panel.set)
+table.pack(expand=tk.YES, fill=tk.BOTH, anchor='w')
+
+txt_title_spl_update = ttk.Label(frame_control, text="Обновление данных в таблице")
+txt_entry_id = ttk.Label(frame_control, text="Введите ID сеанса")
+txt_entry_new_spl_name = ttk.Label(frame_control, text="Введите новое название SPL")
+entry_id_label=ttk.Entry(frame_control)
+entry_new_spl_name=ttk.Entry(frame_control)
+button_id = ttk.Button(frame_control, text='Обновить данные в БД', command=clear_data_in_db)
 
 
-# heads=['id', 'SHOW_NAME', 'SPL_TITLE']
-# table = ttk.Treeview(data_table)
-# table['colums']=heads
-# for header in heads:
-#     table.heading(header, text=header, anchor='center')
-#     table.column(header, anchor='center')
-# for row in data_table:
-#     table.insert("", tk.END, values=row)
-#
-# table.pack(expand=tk.YES, fill=tk.BOTH)
-
-
-
+txt_title_spl_update.grid(row="0", column="1",padx=3,pady=3)
+txt_entry_id.grid(row="1", column="0" ,padx=3,pady=3)
+txt_entry_new_spl_name.grid(row="2", column="0" ,padx=3,pady=3)
+entry_id_label.grid(row='1', column='1')
+entry_new_spl_name.grid(row='2', column='1')
+button_id.grid(row="3", column="1" ,padx=3,pady=3)
 
 window.mainloop()
