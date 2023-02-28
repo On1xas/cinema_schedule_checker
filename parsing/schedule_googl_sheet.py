db2755e538c74b0920727981d1616c75916ed6c5
YAUHEN ALEINIKAU <96020729+On1xas@users.noreply.github.com>
2023-02-28 17:31:27 +0300

    Добавление данных в БД Гугл Таблицы

--- parsing/schedule_googl_sheet.py
+++ parsing/schedule_googl_sheet.py
@@ -1,10 +1,16 @@
 from parsing.parse_cinema_api import *
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
-
+import time
 gs = gspread.service_account(filename='gs_credentials.json')  # подключаем файл с ключами и пр.
 sh = gs.open_by_key('17C7Ch7S9a3vHUIW3B8wGl7b_OYk9MuBRcssG6rbAGVQ')  # подключаем таблицу по ID
 worksheet = sh.sheet1  # получаем первый лист
 res=worksheet.get_all_records()
-worksheet.
+
+for item in parse_schedule_api():
+    print(item)
+    worksheet.append_row(item)
+
+time.sleep(5)
+print(len(worksheet.col_values(1)))
+for row in range(0, len(worksheet.col_values(1))):
+    worksheet.delete_rows(1)
