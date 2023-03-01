ed9b12da520284fb1cecfae0de0b6104f66eacca
e.aleynikov <on1xas509@gmail.com>
2023-03-01 16:00:32 +0300

    добавили путь к папке конфига для импорта

--- application/app_func.py
+++ application/app_func.py
@@ -1,47 +1,56 @@
 import datetime
 import sqlite3
 import xml.etree.ElementTree as ET
-from config.config import *
+import sys
+import os
+
+sys.path.append(f'{os.getcwd()}' + "\\config")
+from config import parsing_url_api
 import colorama
 import requests
 from lxml import etree
 
-def update_pivot_raw(spl_name,id):
+
+def update_pivot_raw(spl_name, id):
     with sqlite3.connect('data\database.db') as db:
         cursor = db.cursor()
         sql = f'UPDATE PIVOT_SHOW_TABLE SET SPL_TITLE_NAME = ? WHERE id = ?'
-        cursor.execute(sql,(spl_name,id))
+        cursor.execute(sql, (spl_name, id))
         db.commit()
+
 
 def get_pivot_info():
     with sqlite3.connect('data\database.db') as db:
-        cursor=db.cursor()
-        sql=f'SELECT * FROM PIVOT_SHOW_TABLE'
+        cursor = db.cursor()
+        sql = f'SELECT * FROM PIVOT_SHOW_TABLE'
         cursor.execute(sql)
-        data=cursor.fetchall()
+        data = cursor.fetchall()
         return data
+
 
 def clear_table(table):
     for item in table.get_children():
         table.delete(item)
     print('Таблица была очищена')
 
+
 def load_table_info(table):
-    data=get_pivot_info()
+    data = get_pivot_info()
     for item in data:
         table.insert("", 'end', values=item)
 
+
 def delete_select_raw(table):
-    item=table.selection()
+    item = table.selection()
     table.delete(item)
 
-def get_show_from_api(dates=datetime.datetime.now().date()):
 
+def get_show_from_api(dates=datetime.datetime.now().date()):
     data_parsing_api = []
     colorama.init()
     url = f'{parsing_url_api}{dates}'
     print(
-            f"***Выполняю запрос сеансов на {colorama.Fore.RED} {dates} {colorama.Style.RESET_ALL} из программного обеспечения кинотеатра***")
+        f"***Выполняю запрос сеансов на {colorama.Fore.RED} {dates} {colorama.Style.RESET_ALL} из программного обеспечения кинотеатра***")
     request = requests.get(url)
     if request.status_code == 200:
         print(f'***Ответ получен успешно***')
@@ -51,31 +60,32 @@ def get_show_from_api(dates=datetime.datetime.now().date()):
         for i in range(1, len(root[0])):
             date = datetime.datetime.strptime(root[0][i][2].text, '%Y-%m-%dT%H:%M:%S')
             data_parsing_api.append(("Данные отсутствуют",
-                                    root[0][i][29].text,
-                                    root[0][i][28].text,
-                                    root[0][i][15].text.strip(),
-                                    root[0][i][40].text,
-                                    "Данные отсутствуют",
-                                    "ИСТИНА",
-                                    root[0][i][2].text,
-                                    "Данные отсутствуют",
-                                    "Данные отсутствуют",
-                                    "Данные отсутствуют",
-                                    "Данные отсутствуют",
-                                    str(date.date()),
-                                    "Данные отсутствуют",
-                                    root[0][i][34].text,
-                                    "Dolby Digital",
-                                    "Русский язык",
-                                    "Данные отсутствуют",
-                                    root[0][i][20].text,
-                                    root[0][i][24].text
-                                    ))
+                                     root[0][i][29].text,
+                                     root[0][i][28].text,
+                                     root[0][i][15].text.strip(),
+                                     root[0][i][40].text,
+                                     "Данные отсутствуют",
+                                     "ИСТИНА",
+                                     root[0][i][2].text,
+                                     "Данные отсутствуют",
+                                     "Данные отсутствуют",
+                                     "Данные отсутствуют",
+                                     "Данные отсутствуют",
+                                     str(date.date()),
+                                     "Данные отсутствуют",
+                                     root[0][i][34].text,
+                                     "Dolby Digital",
+                                     "Русский язык",
+                                     "Данные отсутствуют",
+                                     root[0][i][20].text,
+                                     root[0][i][24].text
+                                     ))
         return list(sorted(data_parsing_api, key=lambda x: x[2]))
     else:
         print(
-                f"""***Проверьте полученные данные, возможно запрос был выполнен некорректно или не выполнен вовсе. Ошибка запроса {request.status_code}.***""")
+            f"""***Проверьте полученные данные, возможно запрос был выполнен некорректно или не выполнен вовсе. Ошибка запроса {request.status_code}.***""")
         return None
+
 
 if __name__ == '__main__':
     get_show_from_api()
--- application/application.py
+++ application/application.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 from app_func import update_pivot_raw,clear_table,load_table_info,delete_select_raw
-from config.config import *
 
+import sys
+
+sys.path.append('../')
 def clear_data_in_db():
     new_id=entry_id_label.get()
     new_spl=entry_new_spl_name.get()
