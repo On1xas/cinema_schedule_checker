a02ec255a9ab1023633d900c4c778008a5e080e9
YAUHEN ALEINIKAU <96020729+On1xas@users.noreply.github.com>
2023-03-11 23:16:18 +0300

    Реализован парсинг через приложение

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import sqlite3
 import tkinter as tk
 from tkinter import ttk
 from tkcalendar import DateEntry, Calendar
-from application.app_func import update_pivot_raw, clear_table, load_table_info, delete_select_raw, get_pivot_info
+from application.app_func import update_pivot_raw, clear_table, load_table_info, delete_select_raw, start_parse
+from parsing.schedule_googl_sheet import add_show_gsdb
+
+Param = dict()
 
 
 class App(tk.Tk):
@@ -17,129 +20,147 @@ class App(tk.Tk):
 
 class User(App):
 
-    def clear_table(self):
-        for item in self.get_children():
-            self.delete(item)
-        print('Таблица была очищена')
-
-    def clear_data_in_db(self):
-        update_pivot_raw(*self.get_id_and_spl_name())
-        clear_table(self)
-        load_table_info(self)
-        self.delete_entry_id_spl()
-
-    def get_id_and_spl_name(self):
-        id = self.entry_id_label.get()
-        spl = self.entry_new_spl_name.get()
-        return (spl, id)
-
-    def delete_entry_id_spl(self):
-        self.entry_id_label.delete(0, tk.END)
-        self.entry_new_spl_name.delete(0, tk.END)
-
-    def clear_table(self):
-        for item in self.table.get_children():
-            self.delete(item)
-        print('Таблица была очищена')
-
-    def load_table_info(self):
-        data = get_pivot_info()
-        for item in data:
-            self.insert("", 'end', values=item)
+    @staticmethod
+    def clear_data_in_db():
+        update_pivot_raw(Param["new_spl"], Param["id"])
+        clear_table(Param['main_table'])
+        load_table_info(Param['main_table'])
 
 
 class Frame_table(tk.Frame, User):
+
     def __init__(self, window):
         super().__init__(window)
         self['background'] = 'yellow'
+        self.table = ttk.Treeview(self, show='headings')
+        Param['main_table'] = self.table
         self.put_widgets()
+    def popup(self,event):
+        item=self.table.selection()
+        row=self.table.item(item)
+        if row['values'] !="":
+            self.menu.post(event.x_root, event.y_root)
 
     def put_widgets(self):
-        #     # Создаем виджет Treeview (аналог таблицы)
-        table = ttk.Treeview(self, show='headings')
-        #
         #     # Задаем название стобцов
         heads = ['id', 'SHOW_NAME', 'SPL_TITLE']
         #     # Присватваем параметру "colums" название столбцов
-        table['columns'] = heads
-        #
+        self.table['columns'] = heads
         #     # Передаем данные в таблицу. Вносим заголовки столбцов и выравниваем их по центру
         for head in heads:
-            table.heading(head, text=head, anchor='center')
+            self.table.heading(head, text=head, anchor='center')
             # Передаем команду что данные в ячейках столбцов должна быть поцентру
-            table.column(head, anchor='center')
+            self.table.column(head, anchor='center')
 
-        load_table_info(table)
+        load_table_info(self.table)
         #
         scr_panel = ttk.Scrollbar(self)
         scr_panel.pack(side=tk.RIGHT, fill=tk.Y)
-        table.configure(yscrollcommand=scr_panel.set)
-        table.pack(expand=tk.YES, fill=tk.BOTH, anchor='w')
-    #
+        self.table.configure(yscrollcommand=scr_panel.set)
+        self.table.pack(expand=tk.YES, fill=tk.BOTH, anchor='w')
+        self.table.bind("<Button-3>", self.popup)
+        self.menu = tk.Menu(tearoff=0)
+        self.menu.add_command(label="Изменить SPL_TITLE", command=Change_spl_title_window)
+        self.menu.add_command(label="Удалить", command=self.button_delete_raw)
 
+    #
+    def button_delete_raw(self):
+        delete_select_raw(Param['main_table'])
 
 class Frame_control(tk.Frame, User):
 
     def __init__(self, window):
         super().__init__(window)
-        self['background'] = 'black'
-        self.put_widgets()
+        # self.put_widgets()
 
-    def put_widgets(self):
-        #     ### БЛОК ИЗМЕНЕНИЯ НАЗВАНИЯ ПЛЕЙЛИСТА
-        #     ### ----------------------------------------------------------------------------------------
+    # def put_widgets(self):
+        # self.txt_title_spl_update = ttk.Label(self, text="Обновление данных в таблице")
+        # self.txt_entry_id = ttk.Label(self, text="Введите ID сеанса")
+        # self.txt_entry_new_spl_name = ttk.Label(self, text="Введите новое название SPL")
+        # self.entry_id_label = ttk.Entry(self)
+        # self.entry_new_spl_name = ttk.Entry(self)
+        # self.button_id = ttk.Button(self, text='Обновить данные в БД', command=)
+        #
+        # self.txt_title_spl_update.grid(row="0", column="1", padx=3, pady=3)
+        # self.txt_entry_id.grid(row="1", column="0", padx=3, pady=3)
+        # self.txt_entry_new_spl_name.grid(row="2", column="0", padx=3, pady=3)
+        # self.entry_id_label.grid(row='1', column='1')
+        # self.entry_new_spl_name.grid(row='2', column='1')
+        # self.button_id.grid(row="3", column="1", padx=3, pady=3, sticky="s")
+
+        # self.txt_title_button_clear_raw = ttk.Label(self,
+        #                                             text="Если прокат фильма закончился \n Выберите в таблице строку для удаления")
+        # self.button_id = ttk.Button(self, text='Удалить строку', command=self.button_delete_raw)
+        # self.txt_title_button_clear_raw.grid(row="4", column="1", padx=3, pady=3)
+        # self.button_id.grid(row="5", column="1", padx=3, pady=3)
+
+    # def button_delete_raw(self):
+    #     return delete_select_raw(Param['main_table'])
+
+    #     ### ----------------------------------------------------------------------------------------
+
+    #     ### ----------------------------------------------------------------------------------------
+    #     ### Раздел ОБНОВЛЕНИЯ ГУГЛ ТАБЛИЦЫ ЧБОБО
+        txt_title_update_gs = ttk.Label(self, text="Обновленние ВЫГРУЗКИ в таблице расписания чбобо")
+        txt_calendar_gs = ttk.Label(self, text="\tВыберите дату \n \tдля выгрузки данных")
+        self.calendar_update_gs = DateEntry(self, date_pattern="YYYY-mm-dd")
+        button_update_gs = ttk.Button(self, text="Обновить таблицу", command=lambda : add_show_gsdb(self.calendar_update_gs.get_date()))
+
+        txt_title_update_gs.grid(row="6", column="1", padx=3, pady=3)
+        txt_calendar_gs.grid(row="7", column="0", padx=3, pady=3, sticky='we')
+        self.calendar_update_gs.grid(row="7", column="1", padx=3, pady=3)
+        button_update_gs.grid(row="8", column="1", padx=3, pady=3)
+    #     ### ----------------------------------------------------------------------------------------
+    #     ### Раздел проверки расписания софта и ТМСа
+        txt_title_check_schedule = ttk.Label(self,
+                                             text="Проверка Опубликованных сеансов и расписания в TMS RB")
+        txt_calendar_check_schedule = ttk.Label(self, text="Выберите дату для проверки расписания")
+        self.calendar_check_schedule = DateEntry(self, date_pattern="YYYY-mm-dd")
+        button_check_schedule = ttk.Button(self, text="Запустить проверку", command=lambda : start_parse(self.calendar_check_schedule.get_date()))
+
+        txt_title_check_schedule.grid(row="9", column="1", padx=3, pady=3)
+        txt_calendar_check_schedule.grid(row="10", column="0", padx=3, pady=3, sticky='we')
+        self.calendar_check_schedule.grid(row="10", column="1", padx=3, pady=3)
+        button_check_schedule.grid(row="11", column="1", padx=3, pady=3)
+
+# Создаем виджет Treeview (аналог таблицы)
+class Change_spl_title_window(tk.Toplevel):
+    def __init__(self):
+        super().__init__()
+        self.title('Тут будет название приложения')
+        self.iconbitmap()
+        self.minsize(150, 120)
+        tk.Frame(self)
+        self.put_widjet()
+    def put_widjet(self):
         self.txt_title_spl_update = ttk.Label(self, text="Обновление данных в таблице")
         self.txt_entry_id = ttk.Label(self, text="Введите ID сеанса")
         self.txt_entry_new_spl_name = ttk.Label(self, text="Введите новое название SPL")
         self.entry_id_label = ttk.Entry(self)
         self.entry_new_spl_name = ttk.Entry(self)
-        self.button_id = ttk.Button(self, text='Обновить данные в БД', command=self.get_id)
+        self.button_id = ttk.Button(self, text='Обновить данные в БД', command=self.button_get_id)
 
         self.txt_title_spl_update.grid(row="0", column="1", padx=3, pady=3)
         self.txt_entry_id.grid(row="1", column="0", padx=3, pady=3)
         self.txt_entry_new_spl_name.grid(row="2", column="0", padx=3, pady=3)
         self.entry_id_label.grid(row='1', column='1')
         self.entry_new_spl_name.grid(row='2', column='1')
-        self.button_id.grid(row="3", column="1", padx=3, pady=3)
+        self.button_id.grid(row="3", column="1", padx=3, pady=3, sticky="s")
+        self.entry_id_label.insert(0, self.request_id())
 
-    def get_id(self):
-        id=self.entry_id_label.get()
-        spl=self.entry_new_spl_name.get()
-        return update_pivot_raw(spl,id)
-    #     ### ----------------------------------------------------------------------------------------
-    #     txt_title_button_clear_raw = ttk.Label(self,
-    #                                            text="Если прокат фильма закончился \n Выберите в таблице строку для удаления")
-    #     button_id = ttk.Button(self, text='Удалить строку')
-    #
-    #     txt_title_button_clear_raw.grid(row="4", column="1", padx=3, pady=3)
-    #     button_id.grid(row="5", column="1", padx=3, pady=3)
-    #     ### ----------------------------------------------------------------------------------------
-    #     ### Раздел ОБНОВЛЕНИЯ ГУГЛ ТАБЛИЦЫ ЧБОБО
-    #     txt_title_update_gs = ttk.Label(self, text="Обновленние ВЫГРУЗКИ в таблице расписания чбобо")
-    #     txt_calendar_gs = ttk.Label(self, text="\tВыберите дату \n \tдля выгрузки данных")
-    #     calendar_update_gs = DateEntry(self, date_pattern="YYYY-mm-dd")
-    #     button_update_gs = ttk.Button(self, text="Обновить таблицу")
-    #
-    #     txt_title_update_gs.grid(row="6", column="1", padx=3, pady=3)
-    #     txt_calendar_gs.grid(row="7", column="0", padx=3, pady=3, sticky='we')
-    #     calendar_update_gs.grid(row="7", column="1", padx=3, pady=3)
-    #     button_update_gs.grid(row="8", column="1", padx=3, pady=3)
-    #     ### ----------------------------------------------------------------------------------------
-    #     ### Раздел проверки расписания софта и ТМСа
-    #     txt_title_check_schedule = ttk.Label(self,
-    #                                          text="Проверка Опубликованных сеансов и расписания в TMS RB")
-    #     txt_calendar_check_schedule = ttk.Label(self, text="Выберите дату для проверки расписания")
-    #     calendar_check_schedule = DateEntry(self, date_pattern="YYYY-mm-dd")
-    #     button_check_schedule = ttk.Button(self, text="Запустить проверку")
-    #
-    #     txt_title_check_schedule.grid(row="9", column="1", padx=3, pady=3)
-    #     txt_calendar_check_schedule.grid(row="10", column="0", padx=3, pady=3, sticky='we')
-    #     calendar_check_schedule.grid(row="10", column="1", padx=3, pady=3)
-    #     button_check_schedule.grid(row="11", column="1", padx=3, pady=3)
+    def request_id(self):
+        item = Param['main_table'].selection()
+        p_id = Param['main_table'].item(item)
+        print(p_id['values'][0])
+        return str(p_id['values'][0])
+    def button_get_id(self):
 
-
-# Создаем виджет Treeview (аналог таблицы)
-
+        Param["id"] = self.entry_id_label.get()
+        Param["new_spl"] = self.entry_new_spl_name.get()
+        self.entry_id_label.delete(0, tk.END)
+        self.entry_new_spl_name.delete(0, tk.END)
+        User.clear_data_in_db()
+        self.destroy()
 
 window = App()
 
--- application/app_func.py
+++ application/app_func.py
@@ -1,12 +1,26 @@
-
+import datetime
 import sqlite3
+
+import colorama as colorama
+
+from data.database_commands import clear_parsing_all_table, prepare_set_show_from_db, update_pivot_table, \
+    fill_pivot_spl_title, insert_data_tms_palazzo_sql, insert_data_tms_arena_sql, insert_data_tms_dana_sql, \
+    insert_data_tms_triniti_sql
+from parsing.parse_cinema_api import parse_schedule_api
+from config.config import table_th_names, room_names, theatre_names
+from parsing.parsing_arena import parser_tms_arena
+from parsing.parsing_dana import parser_tms_dana
+from parsing.parsing_palazzo import parser_tms_palazzo
+from parsing.parsing_triniti import parser_tms_triniti
 
 
 def update_pivot_raw(spl_name, id):
     with sqlite3.connect('data\database.db') as db:
+        print("Connected DB")
         cursor = db.cursor()
         sql = f'UPDATE PIVOT_SHOW_TABLE SET SPL_TITLE_NAME = ? WHERE id = ?'
         cursor.execute(sql, (spl_name, id))
+        print("commit")
         db.commit()
 
 
@@ -21,6 +35,7 @@ def get_pivot_info():
 
 def clear_table(table):
     for item in table.get_children():
+        print(item)
         table.delete(item)
     print('Таблица была очищена')
 
@@ -28,6 +43,7 @@ def clear_table(table):
 def load_table_info(self):
     data = get_pivot_info()
     for item in data:
+        print(item)
         self.insert("", 'end', values=item)
 
 
@@ -37,11 +53,86 @@ def delete_select_raw(table):
     print(f"{p_id['values'][1]} УДАЛЕН ИЗ DB")
     with sqlite3.connect('data\database.db') as db:
         cursor = db.cursor()
-        sql='DELETE FROM PIVOT_SHOW_TABLE WHERE SHOW_NAME_API = ?'
+        sql = 'DELETE FROM PIVOT_SHOW_TABLE WHERE SHOW_NAME_API = ?'
         cursor.execute(sql, (p_id['values'][1],))
         db.commit()
     table.delete(item)
 
  #   print(, p_id['values'][1]) # точка удаления строки из БД [38, ' Снежная королева: Разморозка ', 'SQ', '2023-02-22 14:47:05.334527']
+def check_show(theatre, rooms, cursor):
+    colorama.init()
+    for room in range(1, rooms + 1):
+        show_api = []
+        show_tms = []
+        print(f'{"*" * 10} ПРОВЕРКА КИНОТЕАТРА {theatre} ЗАЛ {room}  {"*" * 10}')
+        tmpl = f'SELECT SHOW_NAME, SHOW_TIME, FORMAT, AUDIO FROM {theatre}_{room}_ROOM_API'
+        cursor.execute(tmpl)
+        for show in cursor.fetchall():
+            show_api.append(
+                (show[0], show_dict[show[0]], str(datetime.datetime.strptime(show[1], '%Y-%m-%dT%H:%M:%S')), show[2],
+                 show[3]))
 
+        tmpl = f'SELECT SPL_TITLE, SHOW_START FROM {theatre}_{room}_ROOM_TMS'
+        cursor.execute(tmpl)
+        for show in cursor.fetchall():
+            show_tms.append((show[0],
+                             str(datetime.datetime.strptime(show[1][:19], '%Y-%m-%dT%H:%M:%S') + datetime.timedelta(
+                                 minutes=10))))
+        for show in show_api:
+            error_reason = ''
+            for showtms in show_tms:
+                flag_spl = show[1] in showtms[0]
+                flag_time = show[2] == showtms[1]
+                if flag_time and flag_spl:
+                    print(
+                        colorama.Fore.GREEN + f'Сеанс {show[0]} в {show[2][11:-3]} GOOD {colorama.Fore.BLUE if show[3] == "3D" else colorama.Fore.GREEN} {show[3]} {colorama.Fore.GREEN if show[4] == "RU-RU" else colorama.Fore.RED} {show[4]}')
 
+                    print(colorama.Style.RESET_ALL, end='')
+                    break
+                if flag_time == True and flag_spl == False:
+                    error_reason = 'Время сеанса корректное. Неверный SPL или название SPL в базе.'
+            else:
+                if error_reason == "":
+                    error_reason = 'Неверное время сеанса в TMS или сеанса нет в TMS'
+                print(
+                    colorama.Fore.RED + f'Сеанс {show[0]} в {show[2][11:-3]} BAD. {colorama.Fore.BLUE if show[3] == "3D" else colorama.Fore.GREEN} {show[3]} {colorama.Fore.YELLOW if show[4] == "RU-RU" else colorama.Fore.RED} {show[4]} {colorama.Fore.RED} REASON: {error_reason}')
+                print(colorama.Style.RESET_ALL, end='')
+def start_parse(date, show_set_api=set()):
+    connect_db = sqlite3.connect('data\database.db')
+    cur = connect_db.cursor()
+    # # Очистка таблиц от предыдущих данных сеансов
+    clear_parsing_all_table(cur, connect_db)
+    # # Подготавливаем множество названий сеансов которое знает база данных
+    show_set_db = prepare_set_show_from_db(cur)
+    # Запрашиваем сеансы из программного обеспечения букера
+    parse_api = parse_schedule_api(date)
+    # Вносим данные в таблицы залов и ищем новые сеансы о которых не знает система, добавляем
+    for show in parse_api:
+        # Подготавливаем SQL запрос на добавления данных в таблицу залов
+        sql = f"INSERT INTO {table_th_names[theatre_names.index(show[0])]}_{room_names[theatre_names.index(show[0])].index(show[1]) + 1}_ROOM_API (TH_NAME, ROOM, SHOW_TIME, SHOW_NAME, FORMAT, AUDIO) VALUES(?,?,?,?,?,?) "
+        show_set_api.add(show[3])
+        # Отправляем запрос INSERT в БД и передаем в него кортеж сеанса
+        cur.execute(sql, show)
+        connect_db.commit()
+    # Обновляем сводную таблицу. При нахождении новых сеансов, добавляем их в таблицу.
+    update_pivot_table(show_set_api, show_set_db, cur, connect_db)
+    # Заполняем графу SPL_TITLE в сводной таблице.
+    fill_pivot_spl_title(cur, connect_db)
+    # Выполняем парсинг серверов TMS и вносим данные в БД по залам
+    insert_data_tms_palazzo_sql(parser_tms_palazzo(), cur, connect_db,date)
+    insert_data_tms_arena_sql(parser_tms_arena(), cur, connect_db,date)
+    insert_data_tms_dana_sql(parser_tms_dana(), cur, connect_db,date)
+    insert_data_tms_triniti_sql(parser_tms_triniti(), cur, connect_db,date)
+    # # Подготавливаем словарь с названием фильма = название SPL
+    show_dict = dict()
+    tmpl = f'SELECT SHOW_NAME_API,SPL_TITLE_NAME FROM PIVOT_SHOW_TABLE'
+    cur.execute(tmpl)
+    for show in cur.fetchall():
+        show_dict[show[0]] = show[1]
+    # Делаем сверку сеансов кинотеатров
+    check_show('PALAZZO', len(room_names[0]), cur)
+    check_show('ARENA', len(room_names[1]), cur)
+    check_show('DANA', len(room_names[2]), cur)
+    check_show('TRINITI', len(room_names[3]), cur)
+
+    connect_db.close()
